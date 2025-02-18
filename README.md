fabd07ebe9a3658ca96ac8d1836659d0cac2ae0f
Yize Z <yizezhao@YizMac.local>
2025-02-18 01:13:01 -0800

    Prepare release with updated README and specific tokenizer files

--- README.md
+++ README.md
@@ -1,8 +1,48 @@
-# Next_Token_yz
-next token prediction with small language datasets
+# Implicit Geometry of Next-token Prediction: From Language Sparsity Patterns to Model Representations
 
+## Abstract
+
+Next-token prediction (NTP) over large text corpora has become the go-to paradigm to train large language models. Yet, it remains unclear how NTP influences the mapping of linguistic patterns to geometric properties of the resulting model representations. We frame training of large language models as soft-label classification over sparse probabilistic label vectors, coupled with an analytical approximation that allows unrestricted generation of context embeddings. This approach links NTP training to rank-constrained, nuclear-norm regularized optimization in the logit domain, offering a framework for analyzing the geometry of word and context embeddings. We demonstrate that NTP implicitly favors learning logits with a sparse plus low-rank structure, which captures co-occurrence frequencies and underlying sparsity patterns. This results in a phenomenon we term **subspace-collapse**, where contexts followed by similar next-token sets align in a shared subspace. We validate our findings through synthetic and small-scale language datasets, highlighting fundamental principles shaping linguistic representations in large-scale models.
+
+## ArXiv Link
+
+For full details, see our paper on [arXiv](https://arxiv.org/abs/2408.15417).
+
+## Issues and Contact
+
+If you have any questions, please create an issue or contact **zhaoyize@ece.ubc.ca**.
 Tokenizers are in \Tokenizer. 
 
-run Dataset.py main to train the tokenizer. 
+## Tokenizer Training
+To train the tokenizer, run the `Dataset.py` script. This script will handle the training of a custom sentencepiece tokenizer on your dataset.
 
-run traincpu.py to train the model. -> init_from = "ufm" for UFM and init_from = "tfm" for transformer. 
+### Steps:
+1. Ensure your dataset is available in the `./data` directory.
+2. Run the following command to train the tokenizer:
+   ```bash
+   python Dataset.py
+   ```
+   This will train the tokenizer and save the model in the `./tokenizer` directory.
+
+## Model Training
+To train the model, you can use the `train.py` script. This script will utilize the pre-tokenized data to train a language model.
+
+### Steps:
+1. Ensure your configuration file `config.json` is set up correctly.
+2. Run the following command to start training:
+   ```bash
+   python train.py --config config.json
+   ```
+   This will start the training process using the specified configuration.
+
+## Visualization
+To create all visualizations, run the `paperallplot.py` script. This script will generate the necessary plots and save them in the `./plots` directory.
+
+### Steps:
+1. Ensure all necessary data files are available in the `./data` directory.
+2. Run the following command to generate the visualizations:
+   ```bash
+   python paperallplot.py
+   ```
+   This will create the visualizations and save them in the `./plots` directory.
+
--- train.py
+++ train.py
@@ -2,7 +2,8 @@
 supports training of
 1. fixed length next token
 2. AR next token
-3. on a relatively small dataset
+3. ufm
+on a relatively small dataset
 '''
 
 import math
@@ -10,72 +11,152 @@ import os
 import time
 from contextlib import nullcontext
 from datetime import datetime
-from functools import partial
+# from functools import partials
 
 import matplotlib.pyplot as plt
+import numpy as np
 
 from Dataset import *
+from utils import *
+
 
 import torch
-from Model import Transformer, FixedLengthModelArgs, FixedLengthMLP, MultiLabelSVM, TFMModelArgs
+from Model import Transformer, FixedLengthModelArgs, FixedLengthMLP, MultiLabelSVM, TFMModelArgs, UFM
 import torch.nn.functional as F
+from torch.optim.lr_scheduler import StepLR, MultiStepLR
 
+import json
+import argparse
+
+# Parse command-line arguments
+parser = argparse.ArgumentParser(description='Train model with configuration file')
+parser.add_argument('--config', type=str, required=True, help='Path to the configuration file')
+args = parser.parse_args()
+
+# Load configuration from JSON file
+with open(args.config, 'r') as f:
+    config = json.load(f)
 
 # system
-device = "cpu"  # examples: 'cpu', 'cuda', 'cuda:0', 'cuda:1' etc., or try 'mps' on macbooks
-dtype = "float32"  # float32|bfloat16|float16
+device = config["device"]  # examples: 'cpu', 'cuda', 'cuda:0', 'cuda:1' etc., or try 'mps' on macbooks
+dtype = config["dtype"]  # float32|float16|float16
 
 # dataset
-batch_size = None
-T = 6
+batch_size = config["batch_size"]
+if_batch=config["if_batch"]
+T = config["T"]
 
-tok_file = './data/verysmallset_pretok.bin'
-s_len = 3
-vocab_size = 152
-tokenizer_file = './tokenizer/tok'+str(vocab_size)+'.model'
+dataset_name = config["dataset_name"]
+# traininf_tag = "_cpu_verysmall_ufmtfm_128_fix_s3"
+# traininf_tag = "mlp_12_layers_colab0523_run3"
+traininf_tag = config["traininf_tag"]
+
+# dataset_name = "verysmallset"
+# dataset_name = "tiny_extracted_m100"
+tok_file = f'./data/{dataset_name}_pretok_word.bin'
+# record_dir = "/Users/yizezhao/Documents/Models/NextToken"
+record_dir = config["record_dir"]
+# record_dir = "/Users/yizezhao/Desktop/ExpPaperV1"
+s_len = config["s_len"]
+# set_s = "equal"
+# set_s = "random"
+set_s = config["set_s"]
+# range_repeat = 3
+range_repeat = config["range_repeat"]
+# save_pretok = None
+
+
+# model
+init_from = config["init_from"] # mlp, tfm or ufm
+
+
+tokenizer_file = f'./tokenizer/tok_{dataset_name}_word.model'
+sp = spm.SentencePieceProcessor(model_file=tokenizer_file)
+vocab_size = sp.get_piece_size()
+
+result_dir = f'{record_dir}/results_{dataset_name}{traininf_tag}'
+figure_dir = f'{record_dir}/figures_{dataset_name}{traininf_tag}'
+for directory in [result_dir, figure_dir]:
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
 bos = 1
 eos = 2
 
-# model
-init_from = "tfm"
+
 
 # mlp specific
-d_encode = 64
-d_hidden = 128
-d_decode = 64
+d_hiddens = config["d_hiddens"]
+# print(d_hiddens)
 
 # tfm specific
-dim = 256
-n_layers = 2
-n_heads = 6
-n_kv_heads = 6
-multiple_of = 4
-dropout = 0.0
-pos_enc = "off"
-max_seq_len = 6
+d_encode = config["d_encode"]
+# d_hidden = 128
+d_decode = config["d_decode"]
+n_layers = config["n_layers"]
+n_heads = config["n_heads"]
+n_kv_heads = config["n_kv_heads"]
+multiple_of = config["multiple_of"]
+dropout = config["dropout"]
+pos_enc = config["pos_enc"]
+max_seq_len = config["max_seq_len"]
+
+#ufm specific1
+# dim = 512
+dim = config["dim"]
 
 # optimizer
-lr = 5e-5
-lammy = 0.5
-
+lr = config["lr"]
+lammy = config["lammy"]
 #training
-max_iteration = 6000
+max_iteration = config["max_iteration"]
+log_interval = config["log_interval"]
 
-tsk = Task(batch_size=batch_size,
+# if solve_by_cvx:
+#     lmm_name = "Lmm by cvxpy"
+# else:
+#     lmm_name = "Lmm by thm"
+save_pretok = f'{result_dir}/sampled_{dataset_name}.npy'
+
+if init_from == "ufm":
+    if config["balanced_toy"]:
+        tsk = Task(batch_size=batch_size,
+        T=T, tok_file=tok_file, s_len=s_len, vocab_size=vocab_size, bos=bos, eos=eos,
+        device=device, if_ufm=True, x_type=torch.FloatTensor, balanced=True, if_batch=if_batch, set_s=set_s, repeat_range=range_repeat, save_pretok=save_pretok)
+    else:
+        tsk = Task(batch_size=batch_size,
+        T=T, tok_file=tok_file, s_len=s_len, vocab_size=vocab_size, bos=bos, eos=eos,
+        device=device, if_ufm=True, x_type=torch.FloatTensor,if_batch=if_batch, set_s=set_s, repeat_range=range_repeat, save_pretok=save_pretok)
+
+else:
+    tsk = Task(batch_size=batch_size,
     T=T, tok_file=tok_file, s_len=s_len, vocab_size=vocab_size, bos=bos, eos=eos,
-    device=device,)
-# iter_batches = partial(
+    device=device, if_ufm=False, x_type = torch.LongTensor, if_batch=if_batch, set_s=set_s, repeat_range=range_repeat, save_pretok=save_pretok)# iter_batches = partial(
 #     Task.iter_batches, batch_size=batch_size,
 #     T=T, tok_file=tok_file, s_len=s_len, vocab_size=vocab_size, bos=bos, eos=eos,
 #     device=device,)
+
+support_set_pr = tsk.support_set_pr
+support_set_sampled = tsk.support_set_sampled
+ctx_dict = tsk.ctx_dict
 
 #train_batch_iter = iter_batches()
 tsk_entropy = tsk.emp_entropy
 v_ctx = tsk.v_ctx
 v_nt = tsk.v_nt
 train_batch_iter = tsk.iter_batches()
-X, Y = next(train_batch_iter)
-print(X.shape, Y.shape)
+X = enumerate(train_batch_iter)
+# X, Y = X.to(device), Y.to(device)
+# print(X.shape, Y.shape)
+data_n = tsk.n
+data_m = tsk.m
+print(f"Dataset m: {data_m}")
+print(f"Dataset n: {data_n}")
+print(f"Dataset entropy: {tsk_entropy:.4f}")
+print(f"Next Token vocab size: {v_nt}")
+
+S = get_s(tsk.ctx_dict, support_set_sampled, v_nt)
+P = get_p(tsk.ctx_dict, support_set_sampled, support_set_pr, v_nt )
 
 
 #model_args = dict(T=T, v_ctx=vocab_size, v_nt=vocab_size, d_input=d_encode, d_hidden=d_hidden, d_output=d_decode)
@@ -83,10 +164,13 @@ print(X.shape, Y.shape)
 if init_from == "mlp":
     # init a new model from scratch
     print("Initializing a new fixed MLP model from scratch")
+    print(d_hiddens)
     #fixedTconf = FixedLengthModelArgs(**model_args)
-    model = FixedLengthMLP(T=T, v_ctx=v_ctx, v_nt=v_nt, d_input=d_encode, d_hidden=d_hidden, d_output=d_decode)
+    model = FixedLengthMLP(T=T, v_ctx=v_ctx, v_nt=v_nt, d_input=d_encode, d_hiddens=d_hiddens)
+    uniq_emb = tsk.get_unique()
 
 elif init_from == "tfm":
+    print(f'd_decode: {d_decode}')
     model_args = dict(
         dim=d_decode,
         n_layers=n_layers,
@@ -101,132 +185,165 @@ elif init_from == "tfm":
     print("Initializing a new transformer model from scratch")
     gptconf = TFMModelArgs(**model_args)
     model = Transformer(gptconf)
+    uniq_emb = tsk.get_unique()
 
-
+elif init_from == "ufm":
+    # data_m = X.shape[1]
+    model = UFM(m=data_m, d=dim, k=v_nt)
+    print(f'dim: {dim}')
+    print("Initializing a new UFM model from scratch")
+    uniq_emb = tsk.get_unique().type(torch.FloatTensor)
+    # torch.tensor(np.eye(data_m)).type(torch.FloatTensor)
+dummyY = torch.tensor(np.ones(data_m)).type(torch.LongTensor)
+dummyY = dummyY.to(device)
+uniq_emb = uniq_emb.to(device)
 model.to(device)
+model_decode_d = model.output.weight.shape[1]
 # optimizer = torch.optim.SGD(model.parameters(), lr=lr, weight_decay=lammy)
-optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=lammy)
+# scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=np.geomspace(1, max_iteration, num=5), gamma=0.5)
+optimizer = torch.optim.AdamW(model.parameters(), lr=lr, weight_decay=lammy)
+# scheduler = StepLR(optimizer, step_size=2000, gamma=0.5)  # Reduce the learning rate by a factor of 0.1 every 100 epochs
+# verysmall
+gamma = config["gamma"]
+milestones = config["milestones"]
+rec_ = config["rec_"]
+scheduler = MultiStepLR(optimizer, milestones=milestones, gamma=gamma)
+# m100
+# scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[5000, 10000,20000, 22500, 25000], gamma=0.25)
+# m404
+# scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[2500, 4000, 8000,12000,16000, 22500, 25000], gamma=0.5)
 
+# scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[1250, 2500, 4000, 8000,12000,16000, 22500, 25000], gamma=0.5)
+
+
+# uniq_hs = tsk.get_unique()
+# if init_from == "ufm":
+#     uniq_embeds_dict = model.get_embeddings(tsk.ctx_dict)
+# else:
+#     uniq_embeds_dict = model.get_embeddings(uniq_emb, tsk.v_ctx2v,tsk.ctx_dict)
 # optimizer = torch.optim.Adam(model.parameters())
+# all_dict = clean_dict(tsk.ctx_dict, uniq_embeds_dict, support_set_sampled, support_set_pr, tsk.support_set_repeats)
+# print(all_dict)
+# plot_all_dict(sp, all_dict)
+# exit(0)
+
+total_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+print(f"Total trainable parameters: {total_params} for {model.name} model")
+
+#exit(0)
 
 
 losses = []
-W_norm = []
-for iter in range(max_iteration):
-    X, Y = next(train_batch_iter)
+rec_ = [0, 1, 10, 100, 500, 1000]
+W_list, H_list, L_list = [],[],[]
+W_dir = []
+rec, proj_rec = [], []
+L_debug, loss_debug = [], []
+i = 0
+W_f = []
+W_star_list, W_fin_list = [],[]
+WG_list, HG_list = [], []
+train_batch_iter = tsk.iter_batches()
+if config["retrain"]:
+    for iter_ in range(max_iteration):
+        running_loss = 0.0
+        dl = tsk.iter_batches()
+        for batch_idx, (X, Y) in (enumerate(dl)):
+            if if_batch == False:
+                X = X[0]
+                Y = Y[0]
 
-    # forward + backward + optimize
-    if (iter+1)%100==0:
-        # save normalized W
-        print('debugging')
-    logits = model(X,Y)
-    loss = model.last_loss
-    print(f"{iter} | loss {loss:.4f} | entropy {tsk_entropy:.2f} ")
-    if loss < 0:
-        break
-    loss.backward()
-    optimizer.step()
-    losses.append(loss.detach().numpy())
-    W_norm.append(np.linalg.norm\
-                      (model.output.weight.detach().numpy()))
+            X = X.type(tsk.x_type).to(device, non_blocking=True)
+            Y = Y.type(torch.LongTensor).to(device, non_blocking=True)
+            # debug device
+            # print(device)
+            # print(X.device, Y.device)
+            logits = model(X,Y)
+            loss = model.last_loss
+
+            loss.backward()
+            optimizer.step()
+            scheduler.step()
+            optimizer.zero_grad()
+            running_loss += loss.item()
+
+        epoch_loss = running_loss/data_n
+
+        losses.append(epoch_loss)
+
+        if (iter_)%log_interval==0 or iter_ in rec_:
+            # This_W = model.output.weight.detach().numpy().copy()
+            # w_diff_norm = (This_W - Last_W)/(np.linalg.norm(This_W - Last_W)+1e-5)
+            # W_dir.append(w_diff_norm)
+            # W_norm.append((model.output.weight.detach().numpy().copy())/W_norm[-1])
+
+            # H_list.append((model.fc1.weight.detach().numpy().copy()))
+            with torch.no_grad():
+                W_list.append((model.output.weight.cpu().detach().numpy().copy()))
+                L_list.append(model(uniq_emb, dummyY).cpu().detach().numpy().copy())
+                H_list.append(model.forward_embedding(uniq_emb).cpu().detach().numpy().copy())
+                # W_norm.append(np.linalg.norm \
+                #                   (model.output.weight.detach().numpy()))
+                # L_debug = H_list @
+
+            # Last_W = This_W
+            rec.append(iter_)
+            print(f"{iter_} | loss {epoch_loss:.4f} | entropy {tsk_entropy:.4f} ")
+
+    # losses = losses
+    W_list = np.array(W_list)
+    L_list = np.array(L_list)
+    H_list = np.array(H_list)
+
+    # save W_list, L_list, H_list
+    if not os.path.exists(result_dir):
+        os.makedirs(result_dir)
+    np.save(f'{result_dir}/W_list_{model.name}_d{dim}.npy', W_list)
+    np.save(f'{result_dir}/L_list_{model.name}_d{dim}.npy', L_list)
+    np.save(f'{result_dir}/H_list_{model.name}_d{dim}.npy', H_list)
+    np.save(f'{result_dir}/losses_{model.name}_d{dim}.npy', losses)
+    np.save(f'{result_dir}/rec_{model.name}_d{dim}.npy', rec)
+    np.save(f'{result_dir}/P_{model.name}_d{dim}.npy', P)
+
+    #save the model
+    torch.save(model.state_dict(), f'{result_dir}/model_{model.name}.pth')
+
+else:
+    # load back  W_list, L_list, H_list
+    W_list = np.load(f'{result_dir}/W_list_{model.name}_d{dim}.npy')
+    L_list = np.load(f'{result_dir}/L_list_{model.name}_d{dim}.npy')
+    H_list = np.load(f'{result_dir}/H_list_{model.name}_d{dim}.npy')
+    losses = np.load(f'{result_dir}/losses_{model.name}_d{dim}.npy')
+    rec = np.load(f'{result_dir}/rec_{model.name}_d{dim}.npy')
+
+    model.load_state_dict(torch.load(f'{result_dir}/model_{model.name}.pth', map_location=torch.device(device)))
+
+def plot_train():
+    nrows, ncols = 2, 1
+    fig, axs = plt.subplots(nrows, ncols, figsize=(6 * ncols, 4 * nrows), dpi=100)
+
+    axs[0].plot(np.arange(len(losses)), losses - tsk_entropy, label='CE loss')
+    if config["debug"]:
+        axs[0].plot(rec, loss_debug - tsk_entropy, label='debug_loss')
+    # axs[0].axhline(y=tsk_entropy, color='r', linestyle='--', label='Dataset entropy')
+
+    # Add labels and legend
+    axs[0].set_xlabel('Iteration')
+    axs[0].set_ylabel('loss')
+    axs[0].set_yscale('log')
+    axs[0].set_title(f'Cross Entropy loss with {model.name} \n on {dataset_name} \n H={tsk_entropy:.4f}')
+    axs[0].legend()
 
 
 
+    # Show the plot
+    fig.subplots_adjust(wspace=0.5, hspace=0.5)
+    if not os.path.exists(f'{figure_dir}'):
+        os.makedirs(f'{figure_dir}')
+    plt.savefig(f'{figure_dir}/CEloss{model.name}s_'+ str(s_len)+'.pdf')
+    # plt.show()
 
 
-# losses = losses
-plt.plot(np.arange(len(losses)), losses, label='CE loss')
-plt.axhline(y=tsk_entropy, color='r', linestyle='--', label='Dataset entropy')
-
-# Add labels and legend
-plt.xlabel('Iteration')
-plt.ylabel('loss')
-plt.title('Cross Entropy loss with Fixed Length MLP on verysmalldataset S='+ str(s_len))
-plt.legend()
-
-# Show the plot
-plt.savefig('./figures/FixedLengthMLPs_'+ str(s_len)+'.png')
-plt.show()
-
-plt.clf()
-plt.plot(np.arange(len(W_norm)), W_norm, label='W norm')
-# plt.axhline(y=tsk_entropy, color='r', linestyle='--', label='Dataset entropy')
-
-# Add labels and legend
-plt.xlabel('Iteration')
-plt.ylabel('Norm value of W')
-plt.title('W norm with Fixed Length MLP on verysmalldataset S='+ str(s_len))
-plt.legend()
-
-# Show the plot
-plt.savefig('./figures/FixedLengthMLPs_Wnorm_'+ str(s_len)+'.png')
-plt.show()
-
-# exit(0)
-
-# SVM
-
-uniq_hs = tsk.get_unique()
-uniq_embeds_dict = model.get_embeddings(uniq_hs, tsk.v_ctx2v)
-# print(embeds)
-support_set_pr = tsk.support_set_pr
-support_set_sampled = tsk.support_set_sampled
-ctx_dict = tsk.ctx_dict
-if init_from == "mlp":
-    svm = MultiLabelSVM(
-    emb_dict=uniq_embeds_dict, support_dict=support_set_sampled, prob_dict=support_set_pr, d=model.d_output, v_nt=tsk.v_nt)
-elif init_from == "tfm":
-    svm = MultiLabelSVM(
-        emb_dict=uniq_embeds_dict, support_dict=support_set_sampled, prob_dict=support_set_pr, d=d_decode,
-        v_nt=tsk.v_nt)
-
-w_fin = svm.find_Wfin()
-print(w_fin)
-w_star = svm.find_WStar()
-print(w_star)
-
-# set W = W_fin+infW* and compute entropy
-
-svm_losses, plot_x  = [], []
-embeds = model.forward_embedding(X)
-for i in np.logspace(0.1, 3, 20, endpoint=True):
-    W = torch.Tensor(w_fin + i * w_star)
-    logits = embeds@W.T
-    ce_loss = F.cross_entropy(logits, Y)
-    print(f"alpha={i} | loss {ce_loss:.4f} | entropy {tsk_entropy:.2f} ")
-    svm_losses.append(ce_loss.detach().numpy())
-    plot_x.append(i)
-#
-
-# embedding structure
-plt.clf()
-plt.xscale('log')
-plt.plot(plot_x, svm_losses, label='CE loss')
-plt.axhline(y=tsk_entropy, color='r', linestyle='--', label='Dataset entropy')
-
-# Add labels and legend
-
-plt.xlabel('alpha')
-plt.ylabel('loss')
-plt.title('Cross Entropy loss with Fixed Length embedding and SVM verysmalldataset S='+ str(s_len))
-plt.legend()
-
-# Show the plot
-plt.savefig('./figures/FixedLengthEmbedsSVMs_'+ str(s_len)+'.png')
-plt.show()
-
-# embedding geometry
-sim_ctx = [[b'\x03\x00\x0e\x00\x04\x00&\x00\x06\x00' , b'\x03\x00\x0e\x00\x04\x00!\x00\x06\x00'], [b'\x03\x00\x18\x00\x04\x00\n\x00\x05\x00' ,  b'\x03\x00\x08\x00\x04\x00\x1b\x00\x05\x00'], [b'\x03\x00%\x00\x04\x00\x1a\x00\x06\x00' , b'\x03\x00\x08\x00\x04\x00\x13\x00\x06\x00']]
-for k1, k2 in sim_ctx:
-    sim_dist = np.linalg.norm(uniq_embeds_dict[k1] - uniq_embeds_dict[k2])
-    diff_dists = []
-    for k3, h3 in uniq_embeds_dict.items():
-        if k3 == k1 or k3 == k2:
-            continue
-        diff_dists.append(np.linalg.norm(uniq_embeds_dict[k1] - uniq_embeds_dict[k3]))
-        diff_dists.append(np.linalg.norm(uniq_embeds_dict[k2] - uniq_embeds_dict[k3]))
-    diff_dist = np.array(diff_dists).mean()
-    print(f"{k1+k2} | sim_dist {sim_dist:.4f} | diff_dist {diff_dist:.2f} | rate = {sim_dist/diff_dist:.2f}")
 
 
 
